5f1ffe54a4726fd4485324e8ea1995e0153aad0d
jgreenberg3 <jgreenberg@runbox.com>
2020-02-25 12:22:34 -0500

    up

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,7 +4,7 @@ const Navigation = () => {
   return (
     <div>
       <div className="App">
-        <h1>React Router Mini</h1>
+        <h1>React Router Mini - </h1>
         <div>
           <a href="">Home</a>
         </div>
